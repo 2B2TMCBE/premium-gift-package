31c3c71926cbf9c6df098932df340c9d42523cdb
Max Xie <pxie5@ucsce.edu>
2021-02-27 15:39:03 -0800

    register events and commands

--- src/main/java/com/qubemc/premium_gift/Main.java
+++ src/main/java/com/qubemc/premium_gift/Main.java
@@ -4,15 +4,18 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.slf4j.Logger;
 
-public final class Main extends JavaPlugin {
+public final class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         getLogger().info("Premium Gift Enabled");
+        getServer().getPluginManager().registerEvents(this,this);
     }
 
     @Override
