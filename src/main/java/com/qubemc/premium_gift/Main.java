097763958aa796065057ea4e0863bc38370b1a95
Max Xie <pxie5@ucsce.edu>
2021-02-27 12:49:58 -0800

    initial commit

---
+++ src/main/java/com/qubemc/premium_gift/Main.java
@@ -0,0 +1,18 @@
+package com.qubemc.premium_gift;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("Premium Gift Enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getLogger().info("Premium Gift Disabled");
+    }
+}
